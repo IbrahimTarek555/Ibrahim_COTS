31ceb18cc20c6fdfc72cfd02b1a7272ab36ca772
Ibrahim Tarek <ebrahimtarek555@gmail.com>
2023-05-30 23:43:46 +0300

    Updating stepper driver.

--- HAL/6- STEPPER/STEPPER_interface.h
+++ HAL/6- STEPPER/STEPPER_interface.h
@@ -13,6 +13,10 @@
 #define STEPPER_u8ROTATE_CW		1
 #define STEPPER_u8ROTATE_CCW	2
 
+#define STEPPER_u8SPEED_SLOW	10u
+#define STEPPER_u8SPEED_MEDIUM	6u
+#define STEPPER_u8SPEED_FAST	2u
+
 
 typedef struct STEPPER_t
 {
@@ -24,7 +28,11 @@ typedef struct STEPPER_t
 }STEPPER_t;
 
 
-uint8 STEPPER_u8Rotate(const STEPPER_t *Copy_psteppertMotorInfo, uint16 Copy_u16Angle, uint8 Copy_u8Direction);
+uint8 STEPPER_u8RotateWithFixedSpeed(const STEPPER_t *Copy_psteppertMotorInfo, uint16 Copy_u16Angle, uint8 Copy_u8Direction);
+
+uint8 STEPPER_u8RotateWithAngle(const STEPPER_t *Copy_psteppertMotorInfo, uint16 Copy_u16Angle, uint8 Copy_u8Direction, uint8 Copy_u8Speed);
+
+uint8 STEPPER_u8RotateWithRolls(const STEPPER_t *Copy_psteppertMotorInfo, uint8 Copy_u8RollsNumber, uint8 Copy_u8Direction, uint8 Copy_u8Speed);
 
 
 #endif
