e4a111c861b31cf9a3bdc9c4ac9e4b01578e714d
Ibrahim Tarek <ebrahimtarek555@gmail.com>
2023-05-30 23:54:10 +0300

    Updating BIT_MATH.h.

--- Library/BIT_MATH.h
+++ Library/BIT_MATH.h
@@ -1,16 +1,12 @@
 #ifndef	BIT_MATH_H_
 #define BIT_MATH_H_
 
-/*Function like macro to set a bit*/
 #define SET_BIT(VAR,BIT_NUMBER)	((VAR)|=(1<<(BIT_NUMBER)))
 
-/*Function like macro to clear a bit*/
 #define CLR_BIT(VAR,BIT_NUMBER)	((VAR)&=(~(1<<(BIT_NUMBER))))
 
-/*Function like macro to toggle a bit*/
 #define TOG_BIT(VAR,BIT_NUMBER)	((VAR)^=(1<<(BIT_NUMBER)))
 
-/*Function like macro to get a bit*/
 #define GET_BIT(VAR,BIT_NUMBER)	(((VAR)>>(BIT_NUMBER))&1)
 
 #endif
